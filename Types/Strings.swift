d59541a878992640e44fc2f6fdd77eaea937c689
Patrick Hart <pathart612@gmail.com>
2022-11-23 16:32:21 -0500

    Substrings

--- Types/Strings.swift
+++ Types/Strings.swift
@@ -21,3 +21,20 @@ of an engineer
 let name = "Taylor"
 let age = "26"
 let message = "I'm \(name) and I'm \(age) years old."
+
+
+var name = "Elizabeth"
+
+name.replaceSubrange(bounds: Range<String.Index>, with: Collection)
+
+let replaced = name.replacingOccurrences(of: "Eliza", with "Mac") ==> "Macbeth"
+
+
+// Ranges
+var str = "Hello, playground"
+// character
+str[str.startIndex] // H
+str[str.endIndex]   // error: after last character
+// range
+let range = str.startIndex..<str.endIndex
+str[range]  // "Hello, playground"
